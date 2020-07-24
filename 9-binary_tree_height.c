c8fd1d7b3a83f7eb285f3c8590fa1c9528f98931
achrefbs <siachrefboularess@gmail.com>
2020-07-24 14:58:43 +0100

    task 9

---
+++ 9-binary_tree_height.c
@@ -0,0 +1,27 @@
+#include "binary_trees.h"
+/**
+*binary_tree_height - binary_tree_height
+*@tree: tree to check
+*Return: Return the height of a tree
+*/
+size_t binary_tree_height(const binary_tree_t *tree)
+{
+	size_t count_right = 0, count_left = 0;
+
+	if (tree == NULL)
+		return (0);
+	while (tree->right != NULL)
+	{
+		count_right++;
+		tree = tree->right;
+	}
+	while (tree->left != NULL)
+	{
+		count_left++;
+		tree = tree->left;
+	}
+	if (count_right > count_left)
+		return (count_right);
+	else
+		return (count_left);
+}
